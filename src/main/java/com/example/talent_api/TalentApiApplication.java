26489d8d5fec9e8b717137d31322d8a08e37f7e3
Ritu Sinha <ritusinha128@gmail.com>
2024-08-30 15:47:15 +0000

    initial project setup

---
+++ src/main/java/com/example/talent_api/TalentApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.talent_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class TalentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentApiApplication.class, args);
+	}
+
+}
