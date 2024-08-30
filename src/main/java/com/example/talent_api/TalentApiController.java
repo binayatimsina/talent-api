7e0ba2a41949b0dd584d8273944a7bd0ed31c8cd
emsharma <emsharma07@gmail.com>
2024-08-30 17:00:46 +0000

    get endpoint template

--- src/main/java/com/example/talent_api/TalentApiController.java
+++ src/main/java/com/example/talent_api/TalentApiController.java
@@ -1,7 +1,9 @@
 package com.example.talent_api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,15 @@ public class TalentApiController {
     public String greeting() {
 		return "This is the talent Api!";
 	}
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "Getting All users!";
+    }
+
+    @GetMapping("/users/{id}")
+    public String getUserById(@PathVariable("id") int id) {
+        return "Getting user by id = "+id;
+    }
     
 }
