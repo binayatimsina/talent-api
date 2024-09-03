1419de57886155aae6941d22b979b26239f3d200
emsharma <emsharma07@gmail.com>
2024-09-03 14:55:21 +0000

    job get APIs

--- src/main/java/com/example/talent_api/controller/JobController.java
+++ src/main/java/com/example/talent_api/controller/JobController.java
@@ -1,10 +1,12 @@
 package com.example.talent_api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,19 @@ public class JobController {
         return jobService.getAlljobs();
     }
 
+    @GetMapping("/{job_id}")
+    public Optional<Job> getJobById(@PathVariable("job_id") int job_id){
+        return jobService.getJobById(job_id);
+    }
+
+    @GetMapping("/open")
+    public List<Job> getJobById(){
+        return jobService.getOpenJobs();
+    }
+
+    @GetMapping("/manager/{mgr_id}")
+    public List<Job> getJobsByManager(@PathVariable("mgr_id") int mgr_id){
+        return jobService.getJobsByManager(mgr_id);
+    }
 
 }
