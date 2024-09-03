73f5eca54d1032bb8f456b9fbe257c2752a60663
Ritu Sinha <ritusinha128@gmail.com>
2024-09-03 18:28:41 +0000

    search candidates by searchTerm

--- src/main/java/com/example/talent_api/repository/CandidateRepository.java
+++ src/main/java/com/example/talent_api/repository/CandidateRepository.java
@@ -1,6 +1,10 @@
 package com.example.talent_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.talent_api.model.Candidate;
@@ -8,4 +12,12 @@ import com.example.talent_api.model.Candidate;
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Long>{
 
+    @Query("SELECT c FROM Candidate c WHERE " +
+        "LOWER(c.full_name) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " + 
+        "LOWER(c.email) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " + 
+        "LOWER(c.address) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " + 
+        "LOWER(c.phone) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " + 
+        "LOWER(c.resume) LIKE LOWER(CONCAT('%', :searchTerm, '%'))"
+    )
+    List<Candidate> searchCandidates (@Param("searchTerm") String searchTerm);
 }
