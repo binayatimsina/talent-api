538cd3701199de26badeed62a4c2f02753931a14
Venkat <venkataramana.pamarthy@gmail.com>
2024-09-03 14:36:09 +0000

    Added manager and candidate APIs

--- src/main/java/com/example/talent_api/controller/CandidateController.java
+++ src/main/java/com/example/talent_api/controller/CandidateController.java
@@ -1,10 +1,16 @@
 package com.example.talent_api.controller;
-
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +31,24 @@ public class CandidateController {
         return candidateService.getAllCandidates();
     }
 
+    @GetMapping("/{id}")
+    public Optional<Candidate> getCandidateById(@PathVariable("id") Long id) {
+        return candidateService.getCandidateById(id);
+    }
+
+    @PostMapping("/")
+    public Object addCandidate(@RequestBody Candidate candidate) {
+        return candidateService.addCandidate(candidate);
+    }
+
+    @PutMapping("/{id}")
+    public Object updateCandidate(@PathVariable("id") Long id, @RequestBody Candidate candidate) {
+        return candidateService.updateCandidate(id, candidate);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCandidate(@PathVariable("id") Long id) {
+        candidateService.deleteCandidate(id);
+    }
+    
 }
